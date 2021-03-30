8959a95ab73e0a9191796667bd9c684aa6483ef9
Ben Vickers <bvickers@vmware.com>
2021-03-30 16:43:16 +0200

    fix e2e test script

---
+++ e2e/cleanup-e2e-test-files.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set +e
+
+rm -r examples/minimal/aaa
+rm -r examples/minimal/bbb
+rm -r examples/minimal/ccc
+rm -r examples/mvp/aaa
+rm -r examples/mvp/bbb
+rm -r examples/mvp/ccc
--- e2e/e2e-manual-test.sh
+++ e2e/e2e-manual-test.sh
@@ -1,14 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 set -euo pipefail
 
 cd examples/minimal
-mkdir aaa; mkdir bbb; mkdir ccc; echo test > {aaa,bbb,ccc}/test
+mkdir aaa; mkdir bbb; mkdir ccc
+echo test > aaa/test
+echo test > bbb/test
+echo test > ccc/test
 prify run; git checkout main
 echo "PRs should have been created from minimal prify.yml, check all is as expected"
 cd -
 
 cd examples/mvp
-mkdir aaa; mkdir bbb; mkdir ccc; echo test > {aaa,bbb,ccc}/test
+mkdir aaa; mkdir bbb; mkdir ccc
+echo test > aaa/test
+echo test > bbb/test
+echo test > ccc/test
 prify run; git checkout main
 echo "PRs should have been created from mvp prify.yml, check all is as expected"
 cd -
